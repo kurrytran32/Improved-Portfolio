0f98fde01015c1aaf94de3a4d830dab2b4cc6d93
kurryderp <kurrytran32@gmail.com>
2018-07-14 00:10:14 -0700

    some more styling for card headers and contact info card

--- assets/javascript/web.js
+++ assets/javascript/web.js
@@ -1,22 +1,17 @@
-// $(document).ready(function(){
-//     //appending images of projects and including links
 
-// })
 
 function projectShow() {
     let projects = {
         projName: ['Trivia Game', 'Word Guess Game', 'The Destinator'],
         projLinks: ['https://kurrytran32.github.io/TriviaGame/', 'https://kurrytran32.github.io/Word-Guess-Game/', 'https://loftusjl.github.io/Project1/'],
         projImages: ['./assets/images/TriviaGame.png', './assets/images/Word-Guess-Game.png', './assets/images/Destinator.png']
-
     }
     let proj = projects;
     for (i = 0; i < proj.projName.length; i++) {
         console.log(i)
         console.log(proj.projName[i])
         console.log(proj.projLinks[i])
-        // $('#proj-port').append(`<p>${proj.projImages[i]}</p>`)
-        // begin structuring carousel items
+        
 
         // structuring carousel items with id based on index value
         let $divC = $('<div>');
@@ -48,3 +43,4 @@ function projectShow() {
         $capDiv.append($link);
     }
 }
+$(document).ready(projectShow)
